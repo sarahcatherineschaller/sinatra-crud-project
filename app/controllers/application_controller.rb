1142f5468033051b21e7363ea55c075c412c149f
sarahschaller <sarcsch@aol.com>
2017-11-30 10:25:08 -0500

    set up application controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,25 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+	configure do 
+		set :public_folder, 'public' 
+		set :views, 'app/views'
+		enable :sessions
+		set :session_secret, 'password_security'
+	end 
+
+	get '/' do 
+		erb :index 
+	end 
+
+	helpers do 
+		def logged_in? 
+			!!session[:employee_id]
+		end 
+
+		def current_employee 
+			@current_employee ||= Employee.find_by(id: session[:employee_id]) if session[:employee_id]
+		end
+	end
+end
