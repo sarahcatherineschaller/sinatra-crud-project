75b1297d1faccd80aeb4d52fc8bb6036cd47ba56
sarahschaller <sarcsch@aol.com>
2017-11-30 10:29:36 -0500

    get signup

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,3 +1,12 @@
 class EmployeesController < ApplicationController 
 
+	get '/signup' do 
+		if logged_in?
+			redirect '/tasks'
+		else 
+			erb :'employees/create_employee'
+		end 
+	end 
+
+
 end
