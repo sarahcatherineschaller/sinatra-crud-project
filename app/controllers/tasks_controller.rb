ef24425bd81c63dc9ef410c52eccc54e9b68ca70
sarahschaller <sarcsch@aol.com>
2017-12-06 15:15:17 -0500

    tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,12 @@
 class TasksController < ApplicationController 
+
+	get '/tasks' do 
+		if logged_in?
+			@task = current_employee.tasks
+			erb :'tasks/tasks'
+		else 
+			redirect '/login'
+		end 
+	end 
+
 end
