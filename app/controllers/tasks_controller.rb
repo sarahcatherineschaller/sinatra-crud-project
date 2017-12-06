02f91848b0dfc83ece7ec84961c3d6686c13f9c2
sarahschaller <sarcsch@aol.com>
2017-12-06 15:43:03 -0500

    update tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,4 +9,21 @@ class TasksController < ApplicationController
 		end 
 	end 
 
+	get '/tasks/new' do 
+		if logged_in?
+			erb :'/tasks/create_task'
+		else
+			redirect '/login'
+		end 
+	end
+
+	post 'tasks' do 
+		if params[:name] == "" || params[:due_date] == ""
+			redirect '/tasks/new'
+		else 
+			employee = current_employee
+			@task = Task.create(name: params[:name], due_date: params[:due_date], employee_id: employee.id)
+		end 
+	end
+
 end
