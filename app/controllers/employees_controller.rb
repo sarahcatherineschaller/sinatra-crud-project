8924ecceeb6055ff5a28176c1b08ebb3550fe1d7
sarahschaller <sarcsch@aol.com>
2017-12-06 13:19:38 -0500

    fix error messages signup

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,43 +2,50 @@ class EmployeesController < ApplicationController
 
 	get '/signup' do 
 		if logged_in?
-			redirect '/tasks'
+			redirect '/show'
 		else 
+			@errors = {}
 			erb :'employees/create_employee'
 		end 
 	end 
 
 	post '/signup' do 
 		@errors = {}
-		if params[:name] = ""
+		if params[:name] == ""
 			@errors[:name] = "Name can't be blank!"
+			redirect '/signup'
 		end
 
-		if params[:username] = ""
+		if params[:username] == ""
 			@errors[:username] = "Username can't be blank!"
+			redirect '/signup'
 		elsif Employee.find_by(username: params[:username])
 			@errors[:username] = "Username has already been taken!"
+			redirect '/signup'
 		end 
 
-		if params[:password] = ""
+		if params[:password] == ""
 			@errors[:password] = "Password can't be blank!"
+			redirect '/signup'
 		end 
 
-		if params[:address] = ""
+		if params[:address] == ""
 			@errors[:address] = "Address can't be blank!"
+			redirect '/signup'
 		end 
 
-		if params[:start_date] = ""
+		if params[:start_date] == ""
 			@errors[:start_date] = "Start Date can't be blank!"
+			redirect '/signup'
 		end 
 
 		if @errors.empty? 
-			@employee = Employee.create(name: params[:name], username: params[:username], password: params[:password], address: params[:address], start_date: params[:start_date])
+			@employee = Employee.create(name: params[:name], username: params[:username], password: params[:password], address: params[:address], start_date: params[:start_date], role_id: params[:role_id])
 			@employee.save 
 			sessions[:employee_id] = @employee.id
 			redirect '/show'
 		else 
-			erb :'employees/create_employee'
+			redirect '/signup'
 		end
 	end
 
@@ -49,4 +56,6 @@ class EmployeesController < ApplicationController
 			erb :'employees/login'
 		end 
 	end
+
+
 end
